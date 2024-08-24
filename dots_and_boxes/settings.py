ec1723649f21995fb06cca663c93d642f3b9c38b
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-08-24 21:18:21 +0330

    fix local server bug

--- dots_and_boxes/settings.py
+++ dots_and_boxes/settings.py
@@ -72,7 +72,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
